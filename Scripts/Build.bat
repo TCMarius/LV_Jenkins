e94651e9597055391300717688032af091b5fbd2
TCMarius <trifan.marius@yahoo.com>
2024-02-27 10:06:38 +0200

    updated lv path

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -1,3 +1,3 @@
 SET cur_dir=%cd%
 echo %cur_dir%
-LabVIEWCLI -LogToConsole true -OperationName ExecuteBuildSpec -PortNumber 3366 -ProjectPath "%cur_dir%\LabviewJenkinsExample.lvproj" -BuildSpecName "App"
+LabVIEWCLI -LogToConsole true -OperationName ExecuteBuildSpec -PortNumber 3366 -LabVIEWPath "C:\Program Files\National Instruments\LabVIEW 2021\LabVIEW.exe" -ProjectPath "%cur_dir%\LabviewJenkinsExample.lvproj" -BuildSpecName "App"
--- Scripts/UnitTest.bat
+++ Scripts/UnitTest.bat
@@ -1,3 +1,3 @@
 SET cur_dir=%cd%
 echo %cur_dir%
-LabVIEWCLI -LogToConsole true -PortNumber 3366 -OperationName RunVI -VIPath "%cur_dir%\Tests\JenkinsUnitTests.vi"
+LabVIEWCLI -LogToConsole true -PortNumber 3366 -LabVIEWPath "C:\Program Files\National Instruments\LabVIEW 2021\LabVIEW.exe" -OperationName RunVI -VIPath "%cur_dir%\Tests\JenkinsUnitTests.vi"
